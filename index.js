dfc73ef37ea78ef5adc18048a93cc844317aaff6
gatomai <40182943+gatomai@users.noreply.github.com>
2018-08-01 17:34:51 -0400

    Update index.js
    Made the function recursive.

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all it's items individually
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  });
+return sum;
 }
 
 module.exports = sumItems;
+// const arr = [1,2,3];
+// console.log(sumItems(arr));
